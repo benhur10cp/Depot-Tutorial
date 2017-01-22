45f021b064c358111f47bbb0d908a051fc052810
Benhur <benhur10cp@gmail.com>
2017-01-22 18:02:33 -0200

    sending mail, (not working i think so)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      user_name: "dave",
+      password: "secret",
+      enable_starttls_auto: true
+    }
+  end
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -55,5 +70,5 @@ Rails.application.configure do
 
   config.action_cable.disable_request_forgery_protection = true
 
-  
+
 end
